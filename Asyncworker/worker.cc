91e876d6d7116b1c0c533af3609a5d9bb2d25b00
alsuga <ing.alsuga@gmail.com>
2014-09-10 17:11:57 -0500

    worker v1

--- Asyncworker/worker.cc
+++ Asyncworker/worker.cc
@@ -3,34 +3,31 @@
 
 using namespace std;
 
-char *itoa(int in){
-  string tmp;
-  char out[20];
-  tmp= itos(in);
-  strcpy(out,tmp.c_str);
+char *itoa(int in,char *out){
+  sprintf(out, "%i\0", in);
   return out;
 }
 
-void dispacher(zmsg_t *in_msg, zmsg_t *out_msg ){
+void dispatcher(zmsg_t *in_msg, zmsg_t *out_msg ){
   zframe_t *wh = zmsg_pop(in_msg);
-  char *tmp = zmsg_popstr(in_msg);
+  char *tmp = zmsg_popstr(in_msg), out[20];
   int a,b;
   assert(tmp);
   zmsg_append(out_msg, &wh);
   if(strcmp(tmp,"+") == 0){
     a = atoi(zmsg_popstr(in_msg));
     b = atoi(zmsg_popstr(in_msg)) + a;
-    zmsg_addstr(out_msg,itoa(b));
+    zmsg_addstr(out_msg,itoa(b,out));
   }
   if(strcmp(tmp,"-") == 0){
     a = atoi(zmsg_popstr(in_msg));
     b = a - atoi(zmsg_popstr(in_msg));
-    zmsg_addstr(out_msg,itoa(b));
+    zmsg_addstr(out_msg,itoa(b,out));
   }
   if(strcmp(tmp,"*") == 0){
     a = atoi(zmsg_popstr(in_msg));
     b = atoi(zmsg_popstr(in_msg)) * a;
-    zmsg_addstr(out_msg,itoa(b));
+    zmsg_addstr(out_msg,itoa(b,out));
   }
   if(strcmp(tmp,"/") == 0){
     a = atoi(zmsg_popstr(in_msg));
@@ -39,31 +36,38 @@ void dispacher(zmsg_t *in_msg, zmsg_t *out_msg ){
       zmsg_addstr(out_msg,"division por cero");
       return;
     }
-    zmsg_addstr(out_msg,itoa(a/b));
+    zmsg_addstr(out_msg,itoa(a/b,out));
   }
   zframe_destroy(&wh);
 }
 
-int main(void) {
+int main(int argc, char** argv) {
   zctx_t* context = zctx_new();
   void* client = zsocket_new(context,ZMQ_DEALER);
   void* server = zsocket_new(context,ZMQ_ROUTER);
   zsocket_bind(server, "tcp://*:5555");
   zsocket_bind(client, "tcp://localhost:5555");
 
-  zmsg_t *outmsg;
-  zframe_t *identity = zsocket_identity(client); 
-  
+  zmsg_t *reportmsg;
+  zframe_t *identity = zframe_new(zsocket_identity(client));
+  zmsg_prepend(reportmsg,&identity);
+  zmsg_addstr(reportmsg,argv[1]);
+  zmsg_send(&reportmsg,client);
+  zmsg_destroy(&reportmsg);
+  zframe_destroy(&identity);
+  zsocket_destroy(&client);
+
+
   int i = 0;
   while(true) {
     zmsg_t *outmsg,*inmsg = zmsg_recv(server);
     // Print the massage on the server's console
-    zmsg_print(msg);
+    zmsg_print(inmsg);
     
     dispatcher(inmsg,outmsg);
-    zmsg_send(outsmg,sever);
+    zmsg_send(&outmsg,server);
     zmsg_destroy(&inmsg);
-    zmsg_destroy(&outmsg)
+    zmsg_destroy(&outmsg);
   }
   zctx_destroy(&context);
   return 0;
