9aff0158f2ecf9c0c129ec12f7ffb6351db40d5b
Alejandro <ing.alsuga@gmail.com>
2014-09-22 16:04:13 -0500

    server

--- Asyncworker/diserver.cc
+++ Asyncworker/diserver.cc
@@ -3,34 +3,60 @@
 
 using namespace std;
 
+unordered_map<string, vector<zframe_t*> > wr;
+
+/******* WORKERS ***********/
+
+void handleWorker(zmsg_t *msg, void *clients){
+  zmsg_print(msg);
+  zframe_t* id = zmsg_pop(msg);
+  char *op = zmsg_popstr(msg);
+  if(strcmp(op,"register") == 0){
+    char *operation = zmsg_popstr(msg);
+    wr[operation].push_back(zframe_dup(id));
+  }
+  if(strcmp(op,"answer") == 0){
+    zmsg_send(&msg,clients);
+  }
+  free(op);
+  zframe_destroy(&id);
+  zmsg_destroy(&msg);
+}
+
+
+/******* Clientes *********/
+
+void handleClient(zmsg_t *msg, void *workers){
+  zmsg_print(msg);
+
+  zframe_t* id = zmsg_pop(msg);
+  char *op = zmsg_popstr(msg);
+  //IMPORTANTE
+  zframe_t* worker = getWorke
+}
+
 int main(void) {
   zctx_t* context = zctx_new();
   void* clients = zsocket_new(context,ZMQ_ROUTER);
-  zsocket_bind(server, "tcp://*:5555");
-  void* worker = zsocket_new(context,ZMQ_DEALER);
+  zsocket_bind(clients, "tcp://*:5555");
+  void* workers = zsocket_new(context,ZMQ_DEALER);
+  zsocket_bind(workers, "tcp://*:4444");
+
+  zmq_pollitem_t items[] = {{workers,0,ZMQ_POLLIN,0},
+                            {clients,0,ZMQ_POLLIN,0}};
+
+  cout<<"Estamos listos!"<<endl;
 
   while(true) {
-    zmsg_t *msg = zmsg_recv(server);
-    zmsg_print(msg);
-
-    zframe_t *identity = zmsg_pop(msg);
-    zframe_t *content = zmsg_pop(msg);
-    assert(content);
-    zmsg_destroy(&msg);
-
-    zclock_sleep(2000);
-
-    if (i % 5 == 0) {
-      cout << "I'll answer this one!\n";
-      zframe_send(&identity, server, ZFRAME_REUSE + ZFRAME_MORE);
-      zframe_send(&content, server, ZFRAME_REUSE);
-    } else {
-      cout << "Skipping request\n";
+    zmq_poll(items, 2,10*ZMQ_POLL_MSEC);
+    if(items[0].revents & ZMQ_POLLIN){
+      zmsg_t *inmsg = zmsg_recv(workers);
+      handleWorker(msg, clients);
     }
-    i++;
-
-    zframe_destroy(&identity);
-    zframe_destroy(&content);
+    if(items[1].revents & ZMQ_POLLIN){
+      zmsg_t *inmsg = zmsg_recv(clients);
+      handleClient(msg,workers);
+    }
   }
   zctx_destroy(&context);
   return 0;
